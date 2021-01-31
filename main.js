cfd428d0e46f853955f2f0cef893a47ea35f9100
Kz <parumu4github@gmail.com>
2021-01-31 18:13:58 +0900

    remove key repeat issue

--- main.js
+++ main.js
@@ -1,138 +1,64 @@
 const notes = {
   "c": 261.626,
-  "C#": 277.183,
+  "c#": 277.183,
   "d": 293.665,
-  "D#": 311.127,
+  "d#": 311.127,
   "e": 329.628,
   "f": 349.228,
-  "F#": 369.994,
+  "f#": 369.994,
   "g": 391.995,
-  "G#": 415.305,
+  "g#": 415.305,
   "a": 440.000,
-  "A#": 466.164,
+  "a#": 466.164,
   "b": 493.883,
-  "B#": 523.251,
+  "b#": 523.251,
 }
 
-function addHooks() {
-  document.body.addEventListener('keydown',
-    event => {
-      const key = event.key
-      const keyUpper = event.key.toUpperCase()
-      if (key === 'c') {
-        console.log(`${keyUpper} Down`)
-        const osc = play(notes[key])
-        document.body.addEventListener('keyup', event => {
-          console.log(`${keyUpper} Up`)
-          osc.stop()
-        })
-      }
-      else if (key === 'C') {
-        console.log(`${keyUpper}# Down`)
-        const osc = play(notes[key + "#"])
-        document.body.addEventListener('keyup', event => {
-          console.log(`${keyUpper}# Up`)
-          osc.stop()
-        })
-      }
-      else if (key === 'd') {
-        console.log(`${keyUpper} Down`)
-        const osc = play(notes[key])
-        document.body.addEventListener('keyup', event => {
-          console.log(`${keyUpper} Up`)
-          osc.stop()
-        })
-      }
-      else if (key === 'D') {
-        console.log(`${keyUpper}# Down`)
-        const osc = play(notes[key + "#"])
-        document.body.addEventListener('keyup', event => {
-          console.log(`${keyUpper}# Up`)
-          osc.stop()
-        })
-      }
-      else if (key === 'e') {
-        console.log(`${keyUpper} Down`)
-        const osc = play(notes[key])
-        document.body.addEventListener('keyup', event => {
-          console.log(`${keyUpper} Up`)
-          osc.stop()
-        })
-      }
-      else if (key === 'E') {
-        console.log(`${keyUpper} Down`)
-        const osc = play(notes['f'])
-        document.body.addEventListener('keyup', event => {
-          console.log(`${keyUpper} Up`)
-          osc.stop()
-        })
-      }
-      else if (key === 'f') {
-        console.log(`${keyUpper} Down`)
-        const osc = play(notes[key])
-        document.body.addEventListener('keyup', event => {
-          console.log(`${keyUpper} Up`)
-          osc.stop()
-        })
-      }
-      else if (key === 'F') {
-        console.log(`${keyUpper}# Down`)
-        const osc = play(notes[key + "#"])
-        document.body.addEventListener('keyup', event => {
-          console.log(`${keyUpper}# Up`)
-          osc.stop()
-        })
-      }
-      else if (key === 'g') {
-        console.log(`${keyUpper} Down`)
-        const osc = play(notes[key])
-        document.body.addEventListener('keyup', event => {
-          console.log(`${keyUpper} Up`)
-          osc.stop()
-        })
-      }
-      else if (key === 'G') {
-        console.log(`${keyUpper}# Down`)
-        const osc = play(notes[key + "#"])
-        document.body.addEventListener('keyup', event => {
-          console.log(`${keyUpper}# Up`)
-          osc.stop()
-        })
-      }
-      else if (key === 'a') {
-        console.log(`${keyUpper} Down`)
-        const osc = play(notes[key])
-        document.body.addEventListener('keyup', event => {
-          console.log(`${keyUpper} Up`)
-          osc.stop()
-        })
-      }
-      else if (key === 'A') {
-        console.log(`${keyUpper}# Down`)
-        const osc = play(notes[key + "#"])
-        document.body.addEventListener('keyup', event => {
-          console.log(`${keyUpper}# Up`)
-          osc.stop()
-        })
-      }
-      else if (key === 'b') {
-        console.log(`${keyUpper} Down`)
-        const osc = play(notes[key])
-        document.body.addEventListener('keyup', event => {
-          console.log(`${keyUpper} Up`)
-          osc.stop()
-        })
-      }
-      else if (key === 'B') {
-        console.log(`${keyUpper}# Down`)
-        const osc = play(notes[key + "#"])
-        document.body.addEventListener('keyup', event => {
-          console.log(`${keyUpper}# Up`)
-          osc.stop()
-        })
-      }
+const repeatGuard = new Set()
+
+function keyDown(event) {
+  const key = event.key
+
+  if (repeatGuard.has(key)) {
+    return
+  }
+
+  const handleKey = (key) => {
+    if (key === key.toUpperCase()) { // if key is upper case
+      key = key.toLowerCase() + "#"
     }
-  )
+    if (key == "e#") key = "f"
+    
+    console.log(`${key.toUpperCase()} Down`)
+    repeatGuard.add(key)
+    const osc = play(notes[key])
+    document.body.addEventListener('keyup', event => {
+      console.log(`${key.toUpperCase()} Up`)
+      repeatGuard.delete(key)
+      osc.stop()
+    })
+  }
+
+  if (key === 'c') handleKey(key)
+  else if (key === 'C') handleKey(key)
+  else if (key === 'd') handleKey(key)
+  else if (key === 'D') handleKey(key)
+  else if (key === 'e') handleKey(key)
+  else if (key === 'E') handleKey(key)
+  else if (key === 'f') handleKey(key)
+  else if (key === 'F') handleKey(key)
+  else if (key === 'g') handleKey(key)
+  else if (key === 'G') handleKey(key)
+  else if (key === 'a') handleKey(key)
+  else if (key === 'A') handleKey(key)
+  else if (key === 'b') handleKey(key)
+  else if (key === 'B') handleKey(key)
+
+
+}
+
+function start() {
+  document.body.addEventListener('keydown', keyDown)
 }
 
 function play(freq) {
